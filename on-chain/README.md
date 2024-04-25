1441c9aecf8b80b14205c6982a81260c11bf6eac
dry914 <>
2024-04-25 13:31:37 +0300

    add on-chain folder

---
+++ on-chain/README.md
@@ -0,0 +1,8 @@
+# SPPS on-chain
+
+## TESTS
+
+```shell
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+```
---
+++ on-chain/contracts/SPPS.sol
@@ -0,0 +1,60 @@
+pragma solidity 0.8.19;
+
+contract SPPS {
+    
+    struct Proof {
+        uint[2]  pi_a;
+        uint[2][2]  pi_b;
+        uint[2]  pi_c;
+    }
+    
+    struct Signal {
+        uint blockNumber;
+        string hash;
+    }
+    
+    struct PeriodProof {
+        uint yield;
+        Proof proof;
+        uint blockNumber;
+        string newBalanceHash; // TODO add period
+    }
+    
+    address[] public traders;
+    mapping(address => Signal[]) public signals;
+    mapping(address => PeriodProof[]) public periodProofs;
+    mapping(address => string) public metaData; // email
+    
+    
+    function newTrader(string calldata _metaData) external {
+        traders.push(msg.sender);
+        metaData[msg.sender] = _metaData;
+    }
+    
+    function addSignal(string calldata newSinal) external {
+        Signal memory sig = Signal(block.number, newSinal);
+        signals[msg.sender].push(sig);
+    }
+    
+    function addPeriodProof(uint256 yield, Proof calldata proof, string calldata balanceHash, uint256 blockNumber) external {
+        PeriodProof memory pr = PeriodProof(yield, proof, blockNumber, balanceHash);
+        periodProofs[msg.sender].push(pr);
+    }
+    
+    function getTradeLen(address trader) external view returns(uint) {
+        return signals[trader].length;
+    }
+    
+    function getProofLen(address trader) external view returns(uint) {
+        return periodProofs[trader].length;
+    }
+    
+    function getTradersCount() external view returns(uint) {
+        return traders.length;
+    }
+    
+    // for demo
+    function changeTradeTime(address trader, uint tradeId, uint tradeBlock) external {
+        signals[trader][tradeId].blockNumber = tradeBlock;
+    }
+}
